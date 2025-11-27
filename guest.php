d31c9f87630ee421a9114c78c0827d72241ceac8
aninisrinivasan <aninisrinivasan@gmail.com>
2025-11-27 17:40:54 +0000

    pagination table

--- guest.php
+++ guest.php
@@ -79,19 +79,22 @@ if (isset($_POST["cancel"])) {
                 <tbody>
                     <?php GuestList($editingId) ?>
                 </tbody>
+
             </table>
+            <br />
+            <ul class="pagination">
+                <li><a href="#">&laquo;</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&raquo;</a></li>
+            </ul>
         </section>
     </div>
 
-    <ul class="pagination">
-        <li><a href="#">&laquo;</a></li>
-        <li class="active"><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">&raquo;</a></li>
-    </ul>
+
 
 </body>
 
--- room.php
+++ room.php
@@ -92,20 +92,18 @@ if (isset($_POST["cancel"])) {
                     <?php RoomList($editingId) ?>
                 </tbody>
             </table>
+            <br />
+            <ul class="pagination">
+                <li><a href="#">&laquo;</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&raquo;</a></li>
+            </ul>
         </section>
     </div>
-
-    <ul class="pagination">
-        <li><a href="#">&laquo;</a></li>
-        <li class="active"><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">&raquo;</a></li>
-    </ul>
-
-
 </body>
 
 </html>
